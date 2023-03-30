a1573e48777ea80b4bf85ee729e471a42152f02f
Muhammad Rayhan Ravianda <rayhanrrav@gmail.com>
2023-03-30 19:56:06 +0700

    test

--- src/App.js
+++ src/App.js
@@ -10,13 +10,7 @@ import { useGLTF } from "@react-three/drei";
 function App() {
   return (
     <div className="h-screen">
-      <Canvas class="h-screen">
-        <OrbitControls />
-        <ambientLight intensity={0.5} />
-        <directionalLight position={(-2, 5, 2)} intensity={1} />
         <Model />
-        {/* <Scene /> */}
-      </Canvas>
     </div>
   );
 }
